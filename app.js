8f92b7f87654cfc4d5b8e1dd2aa97966e8594724
Ekaterina Chikovskaia <ekaterina.chikovskaia@gmail.com>
2022-12-14 16:22:25 +0100

    Update the time and date

--- app.js
+++ app.js
@@ -1,3 +1,55 @@
+function formatDate(someDate) {
+  let date = someDate.getDate();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[someDate.getDay()];
+  let months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  let month = months[someDate.getMonth()];
+  let formattedDate = `${day}, ${date} ${month}`;
+  return formattedDate;
+}
+
+let currentDate = document.querySelector("#date");
+let nowDate = new Date();
+currentDate.innerHTML = formatDate(nowDate);
+
+function formatTime(someTime) {
+  let hours = someTime.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = someTime.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let formattedTime = `${hours}:${minutes}`;
+  return formattedTime;
+}
+
+let currentTime = document.querySelector("#time");
+let nowTime = new Date();
+currentTime.innerHTML = formatTime(nowTime);
+
 function displayWeatherCondition(response) {
   console.log(response.data);
   
